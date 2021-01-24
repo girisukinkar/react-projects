3639ede91ea3cb48afdf284aee071fa371e1c1fb
Girish Sukinkar <girisukinkar@gmail.com>
2021-01-24 23:43:50 +0530

    React two way binding and css styling

--- udemy-course/src/App.js
+++ udemy-course/src/App.js
@@ -1,39 +1,53 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-const App = () => {
+class App extends Component {
 
-  const [personsState, switchPersonsState] = useState({
+  state  =  {
     persons : [
       {name: 'Girish',age: 28},
       {name: 'Dexter',age: 27},
       {name: 'John',age: 24}
     ],
     otherState : 'Some other value'
-  });
-
-  console.log(personsState);
-  const switchNameHandler = () => {
-    switchPersonsState(
-      {
-        persons : [
-          {name:'Giri',age:12},
-          {name:'Dex',age:13},
-          {name:'j',age:11}
-        ]
-      }
-    );
   }
 
-    return (
-      <>
-      <div>Hello</div>
-      <button onClick={switchNameHandler}>Switch the name</button>  
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}> I am the child element</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[0].age}/>
-      </>
+   switchNameHandler = (newName) => {
+   this.setState(
+      {
+        persons : [
+          {name:newName,age:12},
+          {name:'Dex',age:13},
+          {name:'jojo',age:11}
+        ]
+      }
     )
+  };
+
+  nameChangHandler = (event) => {
+    this.setState(
+      {
+        persons : [
+          {name:'Girish',age:12},
+          {name:event.target.value,age:13},
+          {name:'jojo',age:11}
+        ]
+      }
+    )
+  };
+
+  render(){
+
+    return (
+      <div className="App">
+      <div>Hello</div>
+      <button onClick={this.switchNameHandler}>Switch the name</button>  
+      <Person click={this.switchNameHandler.bind(this,'Girish Sukinkar')} name={this.state.persons[0].name} age={this.state.persons[0].age} />
+      <Person click={this.switchNameHandler.bind(this,'Dexter Baxter')} changed={this.nameChangHandler} name={this.state.persons[1].name} age={this.state.persons[1].age}> I am the child element</Person>
+      <Person name={this.state.persons[2].name} age={this.state.persons[0].age}/>
+      </div>
+    )
+  }
   
 }
 
--- udemy-course/src/Person/Person.js
+++ udemy-course/src/Person/Person.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-        <>
-        <p>My name is {props.name} and my age is {props.age}</p>
+        <div className="Person">
+        <p onClick={props.click}>My name is {props.name} and my age is {props.age}</p>
         <p>{props.children}</p>
-        </>
+        <input type="text" onChange={props.changed} value={props.name} />
+        </div>
     )
 }
 
