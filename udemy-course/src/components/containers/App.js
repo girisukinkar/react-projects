83ba73c8a801c9ded80d84293be7d5619bad01ae
Girish Sukinkar <girisukinkar@gmail.com>
2021-04-21 12:58:31 +0530

    ocmponent lifecycle

--- udemy-course/src/components/containers/App.js
+++ udemy-course/src/components/containers/App.js
@@ -1,100 +1,93 @@
-import React, { Component } from 'react';
-import './App.css';
-import Persons from '../Persons/Persons';
-import Validate from '../../Validate';
-import Cockpit from '../Cockpit/Cockpit';
+import React, { Component } from "react";
+import "./App.css";
+import Persons from "../Persons/Persons";
+import Validate from "../../Validate";
+import Cockpit from "../Cockpit/Cockpit";
 
 class App extends Component {
-
-
-  state  =  {
-    persons : [
-      {id: '1', name: 'Girish',age: 28},
-      {id: '2', name: 'Dexter',age: 27},
-      {id: '3', name: 'John',age: 24}
-    ],
-    otherState : 'Some other value',
-    showPersons : false,
-    userString: ''
+  constructor(props){
+    super(props);
+    console.log('[App.js] constructor');
   }
+  state = {
+    persons: [
+      { id: "1", name: "Girish", age: 28 },
+      { id: "2", name: "Dexter", age: 27 },
+      { id: "3", name: "John", age: 24 },
+    ],
+    otherState: "Some other value",
+    showPersons: false,
+    userString: "",
+  };
+
+  static getDerivedStateFromProps(props,state){
+    console.log('[App.js ] Derived State', props);
+    return state;
+  }
+
+  componentDidMount(){
+    console.log('[App.js] Mounted');
+  }
+
+
 
   inputNamChangeHandler = (event) => {
     this.setState({
-      userString : event.target.value
-    })
-  }
+      userString: event.target.value,
+    });
+  };
 
   deleteNameHandler = (id) => {
-
     const persons = [...this.state.persons];
     // persons.splice(persons, id);
-    let p = persons.filter(person => person.id !== id);
-    this.setState({persons : p});
-  }
+    let p = persons.filter((person) => person.id !== id);
+    this.setState({ persons: p });
+  };
 
-
-  nameChangHandler = (event,id) => {
-  
-    const person = this.state.persons.findIndex(p => { return p.id === id } );
+  nameChangHandler = (event, id) => {
+    const person = this.state.persons.findIndex((p) => {
+      return p.id === id;
+    });
     let temp = [...this.state.persons];
-    temp[person].name = event.target.value
-    this.setState(
-      {
-        persons : temp
-      }
-    )
+    temp[person].name = event.target.value;
+    this.setState({
+      persons: temp,
+    });
   };
 
   togglePersonsHandler = () => {
     this.setState({
-      showPersons: !this.state.showPersons
-    })
+      showPersons: !this.state.showPersons,
+    });
   };
 
-  
-
-  render(){
-
+  render() {
+    console.log('[App.js] Render');
     const style = {
-      backgroundColor: 'white',
-      font:'inherit',
-      border:'1px solid',
-      padding:'8px',
-      cursor:'pointer'
-    }
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid",
+      padding: "8px",
+      cursor: "pointer",
+    };
 
     let persons = null;
-    if(this.state.showPersons){
-      // style.backgroundColor = 'red';
-      // style.color = 'white';
-      persons = (
-       
-        <div>
-          
-           {
-             <Persons persons={this.state.persons} clicked={this.deleteNameHandler} changed={this.nameChangHandler} />
-        }
-      </div> 
-      );
+    if (this.state.showPersons) {
+      persons = <div>{<Persons persons={this.state.persons} clicked={this.deleteNameHandler} changed={this.nameChangHandler} />}</div>;
     }
 
-   
     return (
       <div className="App">
-      
-      <input type="text" onChange={(e) => this.inputNamChangeHandler(e)} value={this.state.userString}/>
-      <Validate userString={this.state.userString} />
+        <input type="text" onChange={(e) => this.inputNamChangeHandler(e)} value={this.state.userString} />
+        <Validate userString={this.state.userString} />
 
-      
-      <hr/>
-      <Cockpit style={style} title={this.props.appTitle} persons={this.state.persons} clicked={this.togglePersonsHandler} />
-       {persons}
-      {/* <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>   */}
+        <hr />
+        <Cockpit style={style} title={this.props.appTitle} persons={this.state.persons} clicked={this.togglePersonsHandler} />
+        {persons}
+        {/* <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>   */}
       </div>
-    )
+    );
   }
-  
 }
-
 
 export default App;
