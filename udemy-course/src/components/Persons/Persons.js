012f8ef48c3b1de4a299695edba605069c23a44a
Girish Sukinkar <girisukinkar@gmail.com>
2021-04-21 15:20:34 +0530

    PropTypes in React

--- udemy-course/src/components/Persons/Persons.js
+++ udemy-course/src/components/Persons/Persons.js
@@ -1,9 +1,32 @@
-import React from 'react';
-import Person from './Person/Person';
-const persons = (props) => 
-    props.persons.map((person,index) => {
-        return <Person key={person.id} name={person.name} age={person.age} changed={(event) => props.changed(event,person.id)} click={() => props.clicked(person.id)} />            
-      })
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import Person from "./Person/Person";
+class Persons extends Component {
 
- 
-export default persons;
+  
+  render() {
+    const pers =  this.props.persons.map((person) => {
+     
+        <Person
+          key={person.id}
+          name={person.name}
+          age={person.age}
+          changed={(event) => this.props.changed(event, person.id)}
+          click={() => this.props.clicked(person.id)}
+        />
+  
+    });
+
+    return( pers);
+  }
+}
+
+Persons.propTypes = {
+  name : PropTypes.string,
+  persons : PropTypes.array,
+  changed : PropTypes.func,
+  clicked : PropTypes.func
+}
+
+
+export default Persons;
--- udemy-course/src/components/containers/App.js
+++ udemy-course/src/components/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 import "./App.css";
 import Persons from "../Persons/Persons";
 import Validate from "../../Validate";
@@ -8,17 +9,18 @@ class App extends Component {
   constructor(props){
     super(props);
     console.log('[App.js] constructor');
+    this.state = {
+      persons: [
+        { id: "1", name: "Girish", age: 28 },
+        { id: "2", name: "Dexter", age: 27 },
+        { id: "3", name: "John", age: 24 },
+      ],
+      otherState: "Some other value",
+      showPersons: false,
+      userString: "",
+    };
   }
-  state = {
-    persons: [
-      { id: "1", name: "Girish", age: 28 },
-      { id: "2", name: "Dexter", age: 27 },
-      { id: "3", name: "John", age: 24 },
-    ],
-    otherState: "Some other value",
-    showPersons: false,
-    userString: "",
-  };
+ 
 
   static getDerivedStateFromProps(props,state){
     console.log('[App.js ] Derived State', props);
@@ -30,21 +32,20 @@ class App extends Component {
   }
 
 
-
-  inputNamChangeHandler = (event) => {
+  inputNamChangeHandler(event){
     this.setState({
       userString: event.target.value,
     });
-  };
+  }
 
-  deleteNameHandler = (id) => {
+  deleteNameHandler(id){
     const persons = [...this.state.persons];
     // persons.splice(persons, id);
     let p = persons.filter((person) => person.id !== id);
     this.setState({ persons: p });
-  };
+  }
 
-  nameChangHandler = (event, id) => {
+  nameChangHandler(event, id){
     const person = this.state.persons.findIndex((p) => {
       return p.id === id;
     });
@@ -53,13 +54,13 @@ class App extends Component {
     this.setState({
       persons: temp,
     });
-  };
+  }
 
-  togglePersonsHandler = () => {
+  togglePersonsHandler(){
     this.setState({
       showPersons: !this.state.showPersons,
     });
-  };
+  }
 
   render() {
     console.log('[App.js] Render');
@@ -88,6 +89,10 @@ class App extends Component {
       </div>
     );
   }
+}
+
+App.propTypes = {
+  appTitle : PropTypes.string
 }
 
 export default App;
