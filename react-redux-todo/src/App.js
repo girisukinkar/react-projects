f8d60a9cd07f9549a8fde1f58bb71953e59daa46
Girish Sukinkar <girisukinkar@gmail.com>
2021-06-30 00:22:23 +0530

    configured redux

--- react-redux-todo/src/App.js
+++ react-redux-todo/src/App.js
@@ -1,9 +1,30 @@
-import { useState } from 'react';
+import {useState} from "react";
+import {useSelector, useDispatch} from "react-redux";
+import{
+  increment,
+  decrement,
+  incrementByAmount,
+  selectCount
+} from "./redux/counterSlice";
+
 function App() {
-  const [counter, setCounter] = useState(0)
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+  const [counter, setcounter] = useState(0)
   return (
     <div className="App">
-      {counter}
+      <h1>
+      {count}
+
+      </h1>
+      <input
+      value={counter}
+      aria-label="Set increment amount"
+      onChange={e => setcounter(Number(e.target.value)) }
+    />
+    <button onClick={() => dispatch(incrementByAmount(counter))}>IncrementByAmount</button>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   );
 }
